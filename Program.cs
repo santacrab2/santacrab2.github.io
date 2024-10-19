fcea08d45241128e95e526d882dc1a3ecf4da776
Santacrab2 <skaradiodj@gmail.com>
2024-10-19 08:19:34 -0400

    fix

--- Program.cs
+++ Program.cs
@@ -8,5 +8,4 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddBlazoredModal();
-builder.Services.AddSignalRCore();
 await builder.Build().RunAsync();
