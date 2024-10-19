28eb3435117cecd217f9fbabe75e9d680d2e9e75
Santacrab2 <skaradiodj@gmail.com>
2024-10-19 07:45:10 -0400

    im just gonna push this and hope for the best

---
+++ Hubs/SysbotHub.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using PKHeX.Core;
+namespace santacrab2.github.io
+{
+    public class SysbotHub : Hub
+    {
+        public async Task sendConvert(ShowdownSet set)
+        {
+
+            await Clients.User("sysb").SendAsync("Convert", set);
+        }
+        public async Task ReceiveConvert(PKM pk)
+        {
+            await Clients.User("site").SendAsync("DLPK", pk);
+        }
+    }
+}
